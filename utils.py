f43f769e9155ebd460c71d3134a1bed26709b13c
Ckflicks <109861478+Kichuprobotz@users.noreply.github.com>
2023-06-01 07:04:15 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -465,4 +465,4 @@ async def send_all(bot, userid, files, ident):
             file_id=file.file_id,
             caption=f_caption,
             protect_content=True if ident == "filep" else False,
-            reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('⚔️ 𝖯𝖨𝖱𝖮 𝖴𝖯𝖣𝖠𝖳𝖤𝖲 ⚔️', url="https://t.me/piroxbots") ] ] ))
+            reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('⚔️ BOT UPDATES ⚔️', url="https://t.me/ckflicks") ] ] ))
